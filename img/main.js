71a74c5cf742c53b3a4331a40dfd1d2512116ed0
michelle bautista <alejandra.lopez.is@unipolidgo.edu.mx>
2021-11-19 00:03:37 -0600

    'final'

--- img/main.js
+++ img/main.js
@@ -1,3 +1,16 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  const elementosCarousel = document.querySelectorAll('.carousel');
+  M.Carousel.init(elementosCarousel, {
+      duration: 150,
+      dist: -80,
+      shift: 5,
+      padding: 5,
+      numVisible: 5,
+      indicators: true,
+      noWrap: false
+  });
+});
+
 function main() {
 
 (function () {
@@ -125,7 +138,7 @@ function main() {
         prevEl: '.swiper-button-prev',
       },
       autoplay: {
-        delay: 5500,
+        delay: 11200,
         disableOnInteraction: false,
       },
     });
