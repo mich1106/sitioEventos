7d8d0b58edb8f64db9983e667388dddc99d088ce
michelle bautista <alejandra.lopez.is@unipolidgo.edu.mx>
2021-11-25 14:14:25 -0600

    mapa

--- sendemail.php
+++ sendemail.php
@@ -3,7 +3,7 @@ include "PHPMailer/class.phpmailer.php";
 include "PHPMailer/class.smtp.php";
 
 //Recibir todos los parámetros del formulario
-$para = 'michelopbau11@gmail.com';
+$para = 'contacto@gruponemsa.com';
 $asunto = 'Mensaje enviado desde la web de nemsa.mx';
 
 $name = $_POST['name'];
